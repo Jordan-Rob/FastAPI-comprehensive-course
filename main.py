438fbe2d5fd450c49623c34913124215f662af1a
Jordan Rob <jordanrob709@gmail.com>
2021-11-07 19:36:51 +0300

    Fix - added delete path operation

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -12,14 +13,51 @@ class Post(BaseModel):
     rating: Optional[int] = None
 
 
+my_posts = [{"title":"title of post 1", "content":"content of post 1", "id":1}, 
+    {"title":"title of post 2", "content":"content of post 2", "id":2}
+]
+
+
 @app.get("/")
 def get_home():
     return {"mess": "Hello World"}
 
-@app.post("/createpost")
+
+@app.get("/posts")
+def get_posts():
+    return {'data': my_posts}
+
+
+@app.get("/posts/{id}")
+def get_post(id: int, response: Response):
+    #print(type(id))
+    #post_id = int(id)
+    for post in my_posts:
+        if post["id"] == id:
+            return {"data": post}
+        else:
+            #response.status_code = status.HTTP_404_NOT_FOUND
+            #return {"error":f"post with id:{id} does not exist!"}
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id:{id} does not exist!")
+
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 #def craete_post(payload: dict = Body(...)):
 def create_post(new_post: Post):
-    print(new_post)
-    print(new_post.dict())
+    #print(new_post)
+    #print(new_post.dict())
+    post_dict = new_post.dict()
+    post_dict["id"] = randrange(0, 10000)
+    my_posts.append(post_dict)
     #return {"new post": f"title: {payload['title']} content: {payload['content']}"}
-    return {"data": "new post"}
+    return {"data": post_dict}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    for post in my_posts:
+        if post["id"] == id:
+            my_posts.remove(post)
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
+        else:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id:{id} does not exist!")
