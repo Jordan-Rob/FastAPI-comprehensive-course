289ebb5292ffa578ab7a0d92b1bc58fdbc66ac9f
Jordan Rob <jordanrob709@gmail.com>
2021-11-19 14:44:17 +0300

    added table/model for votes

--- app/models.py
+++ app/models.py
@@ -24,3 +24,9 @@ class User(Base):
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
     
+
+class Vote(Base):
+    __tablename__ = "votes"
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True, nullable=False)
+    post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True, nullable=False)
+    
