059eaec83db9cbdbfab2e1188ef06628855808b1
Jordan Rob <jordanrob709@gmail.com>
2021-11-25 00:47:06 +0300

    Added & setup alembic to handle database migrations | created first revision file

---
+++ alembic/versions/d70ca11ae70e_create_posts_table.py
@@ -0,0 +1,24 @@
+"""create posts table
+
+Revision ID: d70ca11ae70e
+Revises: 
+Create Date: 2021-11-25 00:43:46.809851
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd70ca11ae70e'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
